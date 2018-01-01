894d1ec5830eabe9e6d0943c787d0b54c6d1e0f0
Ben WU <wubin28@gmail.com>
2018-01-01 23:38:00 +0800

    create a project from Spring Initializr

---
+++ src/main/java/com/wubinben/restservicedemo/RestServiceDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wubinben.restservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceDemoApplication.class, args);
+	}
+}
